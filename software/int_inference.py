2b29bc1e2419df31318fe8eaf568f86310b447b9
cheungbh <534660436@qq.com>
2023-12-27 15:22:14 +0800

    simplify inference

--- software/int_inference.py
+++ software/int_inference.py
@@ -36,26 +36,16 @@ from utils.loss import Loss
 def main():
     parser = argparse.ArgumentParser(description='Train network.')
     parser.add_argument('--settings_file', help='Path to settings yaml', required=True)
-    parser.add_argument('--save_dir', "-s", type=str, default='')
     parser.add_argument('--int_dir', type=str, default='int')
 
-    parser.add_argument('--evaluate', '-e', action='store_true', default=False)
-    parser.add_argument('--epochs', type=int, default=300)
-    parser.add_argument('--fixBN_ratio', type=float, default=0.3, help="When to fix BN during quantization training")
     parser.add_argument('--data_path', help='Path to dataset', default="")
     parser.add_argument('--bias_bit', type=int, default=32)
 
     parser.add_argument('--dataset_percentage', '-dp', type=float, default=1)
-    parser.add_argument('--resume', type=str, default='')
     parser.add_argument('--conv1_level', type=int, default=8)
     parser.add_argument('--shift_bit', type=int, default=31)
     parser.add_argument('--load', type=str, default='', help='load model path')
-    parser.add_argument('--auto_resume', action='store_true', help='Resume automatically')
-    parser.add_argument('--min_event', type=int, default=0)
-    parser.add_argument('--generate_int_model', action='store_true', help='No BN loading in the model')
     parser.add_argument('--ana_file', default=None, help='Path for error analyser')
-    parser.add_argument('--lamb', type=float, default=0, help="random drop ratio")
-    parser.add_argument('--gradually', action='store_true', default=False)
 
     parser.add_argument('--drop_area_ratio', type=float, default=0.1)
     parser.add_argument('--drop_event_ratio', type=float, default=0.1)
@@ -64,29 +54,14 @@ def main():
     args = parser.parse_args()
 
     settings_filepath = args.settings_file
-    if args.auto_resume:
-        settings_filepath = os.path.join(args.save_dir, "settings.yaml")
-        print("using stored config: {}".format(settings_filepath))
     settings = Settings(settings_filepath, generate_log=False)
     settings = utils.reinit_settings(settings, args)
 
     int_folder = args.int_dir
     os.makedirs(int_folder, exist_ok=True)
 
-    if args.generate_int_model:
-        args.evaluate = True
-
     with open(settings_filepath, 'r') as stream:
         cfg = yaml.load(stream, yaml.Loader)
-
-    if not args.evaluate and not os.path.exists(args.save_dir) and args.save_dir:
-        os.makedirs(args.save_dir)
-        import shutil
-        shutil.copy(settings_filepath, os.path.join(args.save_dir, "settings.yaml"))
-    if args.save_dir:
-        if not utils.check_trained_folder(args) and "test" not in args.save_dir:
-            print("This folder contains trained model!")
-            sys.exit(1)
 
     if args.data_path != "":
         cfg["dataset"]["dataset_path"] = args.data_path
@@ -97,8 +72,7 @@ def main():
     Dataset = utils.select_dataset(cfg["dataset"]["name"])
     MNIST = True if "MNIST" in cfg["dataset"]["name"] or "Poker" in cfg["dataset"]["name"] else False
 
-    val_dataset = Dataset(cfg, mode="validation", shuffle=True, dataset_percentage=args.dataset_percentage,
-                          min_event=args.min_event)
+    val_dataset = Dataset(cfg, mode="validation", shuffle=False, dataset_percentage=args.dataset_percentage,)
     if cfg["dataset"]["name"] == "Roshambo":
         nr_input_channels = 1
 
@@ -128,30 +102,14 @@ def main():
     param_noBN = sum(p.numel() for p in model.parameters() if len(p.shape) > 1)
     print("Number of parameters except BN: {}".format(param_noBN))
     model.to(settings.gpu_device)
-    # model = torch.nn.DataParallel(model).cuda()
     model = model.cuda()
     baseline_model.param = sum(p.numel() for p in baseline_model.parameters() if len(p.shape) > 1)  # param_noBN
     baseline_model = baseline_model.cuda()
 
-    criterion = Loss(settings, args.lamb)
-
-    # specify different optimizer to different training stage
-    if not args.evaluate:
-        if settings.optimizer == 'adam':
-            optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()),
-                                         lr=settings.init_lr)
-        elif settings.optimizer == 'sgd':
-            optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()),
-                                        lr=settings.init_lr,
-                                        momentum=0.9,
-                                        weight_decay=1e-4)
-        else:
-            raise NotImplementedError
-    else:
-        optimizer = None
+    criterion = Loss()
+    optimizer = None
 
     start_epoch, best_acc1 = utils.load_checkpoint(args, model, optimizer)
-    start_epoch += 1
     cudnn.benchmark = True
 
 
