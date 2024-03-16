c6fa3bd377d087fecdb3fc9c57a78403fda3073a
cheungbh <534660436@qq.com>
2024-03-16 22:04:17 +0800

    fix gfc

--- hardware/template_e2e/gen_code.py
+++ hardware/template_e2e/gen_code.py
@@ -256,24 +256,25 @@ def append_perlayer_code(codes, layer, prev_name, first_layer=False, last_layer=
                 None,
             )
     elif layer["type"] == "linear":
-        func_name = "global_avgpool"
-        def_k_list = [cfg_of(name, k) for k in ["PIC", "POC", "IC", "OC", "H", "W"]]
-        def_v_list = layer["parallelism"] + layer["channels"] + layer["input_shape"]
-        unique_tpl_list = def_k_list.copy()
-        common_arg_list = []  # clear common_arg_list
-        unique_arg_list = [
-            afifo_of(prev_name),
-            tfifo_of(prev_name),
-            "act_out",
-            wbuf_of(name),
-        ]
-        # weight.h wbuf
-        codes["weight.h"] += variable_code_block(
-            name,
-            "w",
-            f"{cfg_of(name, 'PIC')}*CFG_WW",
-            f"{cfg_of(name, 'OC')}][{cfg_of(name, 'IC')}/{cfg_of(name, 'PIC')}",
-        )
+        pass
+        # func_name = "global_avgpool"
+        # def_k_list = [cfg_of(name, k) for k in ["PIC", "POC", "IC", "OC", "H", "W"]]
+        # def_v_list = layer["parallelism"] + layer["channels"] + layer["input_shape"]
+        # unique_tpl_list = def_k_list.copy()
+        # common_arg_list = []  # clear common_arg_list
+        # unique_arg_list = [
+        #     afifo_of(prev_name),
+        #     tfifo_of(prev_name),
+        #     "act_out",
+        #     wbuf_of(name),
+        # ]
+        # # weight.h wbuf
+        # codes["weight.h"] += variable_code_block(
+        #     name,
+        #     "w",
+        #     f"{cfg_of(name, 'PIC')}*CFG_WW",
+        #     f"{cfg_of(name, 'OC')}][{cfg_of(name, 'IC')}/{cfg_of(name, 'PIC')}",
+        # )
     else:
         raise ValueError(f"unknown layer type {layer['type']}")
     tpl_list = unique_tpl_list + common_tpl_list
