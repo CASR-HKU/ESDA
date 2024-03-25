0ed852d39b9cd0ce4686d300c1646fa534192c63
cheungbh <534660436@qq.com>
2024-03-25 10:33:35 +0800

    fix pipeline

--- optimization/pipeline.py
+++ optimization/pipeline.py
@@ -1,5 +1,5 @@
 
-import os
+SCIPOPTDIR = "export SCIPOPTDIR=/tools/scip/SCIPOptSuite-8.0.0-Linux #a1"
 
 HW_CONFIG_PATH = "/vol/datastore/EDSA/eventNetHWConfig"
 
@@ -8,7 +8,7 @@ config_path = "/vol/datastore/eye_tracking/eventHWConfig"
 hw_path = "/vol/datastore/eye_tracking/eventNetHW"
 
 hw = ["zcu102_80res", "zcu102_75res"] #, "zcu102_60res", "zcu102_80res", "zcu102_75res"
-model = ["0324_cfg858_biasBit16"]
+model = ["0324_cfg1551_biasBit16", "0324_cfg4222_biasBit16"]
 
 optim_cmd_sample = "python eventnet.py --model_path {} --hw_path {} --model_name {} --hw_name {} --results_path {}"
 hw_prj_cmd_sample = "python gen_prj.py gen_full --cfg_name {} --cfg_path {} --tpl_dir template_e2e --dst_path {}"
@@ -23,9 +23,12 @@ for h in hw:
         optim_cmds.append(optim_cmd)
         hw_prj_cmds.append(hw_prj_cmd)
 
+print(SCIPOPTDIR)
 
 for cmd in optim_cmds:
     print(cmd)
+
+print("cd ../hardware")
 
 for cmd in hw_prj_cmds:
     print(cmd)
